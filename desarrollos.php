1fd39f6364f525cf448e5a8be3c5996757dce20b
William Huera <whuera@gmail.com>
2016-08-16 22:36:00 -0500

    release 1.3.4 mejoras en CSS, se agrega paginas de cursos / aplicativo rrhh / cursos, temario / software a medida

--- desarrollos.php
+++ desarrollos.php
@@ -4,18 +4,114 @@
     <h1 class="panel-title">Desarrollo de Software a la Medida de los requerimientos de su Empresa</h1>
   </div>
       <div class="panel-body">
-          <div class="panel-body"><img src="images/desarrollo2.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+          <div class="panel-body">
+             <img src="images/mobilpymesfondo5.jpg" class="img-responsive" style="height: 25%; width:25%" alt="Image"> 
+          </div>
+          
+           <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-arrow-circle-right fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">SysBroker</div>
+                                        <div>Sistema para Brokers de seguros, cliente - servidor</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">ver informaci&oacute;n</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-tasks fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">Servicauchos</div>
+                                        <div>Sistema de inventarios y facturaci&oacute;n</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">ver informaci&oacute;n</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-shopping-cart fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">PowerFac</div>
+                                        <div>Sistema de Facturaci&oacute;n electr&oacute;nica (cliente - servidor)</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">ver informaci&oacute;n</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-arrow-circle-right fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">DrPlus</div>
+                                        <div>Sistema m&eacute;dico de gesti&oacute;n y control de pacientes</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">ver informaci&oacute;n</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+          
           <div class="well" style="text-align: justify;">
-   Desarrollamos e integramos herramientas informaticas con tecnologia de vanguardia, trabajamos sobre los mas potentes
+              Desarrollamos e integramos herramientas inform&aacute;ticas con tecnolog&iacute;a de vanguardia, trabajamos sobre los m&aacute;s potentes
    framework de desarrollo (Java / C#), para aplicaciones empresariales de alta transaccionalidad, asi como tambien para 
    empresas medianas y pequeñas (PYMES).
-   Tenemos varios productos ejecutandose en varios clientes:
+   Tenemos varios productos ejecutandose en varios clientes.
+   <!--
    <ul>
        <li>Servicauchos</li>
        <li>DrPlus</li>
        <li>SysBroker</li>
         <li>PowerFac</li>
    </ul>
+   -->
           </div>
   </div>
 
