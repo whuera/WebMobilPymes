5f1d3e3a7744c051d8d66bd26f0a6791aa8946b4
William Huera <whuera@gmail.com>
2016-08-25 22:08:46 -0500

    release 1.3.6 mejoras en CSS, se agrega paginas de cursos / aplicativo rrhh / cursos, temario / software a medida / nuevas paginas

--- cursos.php
+++ cursos.php
@@ -1,3 +1,18 @@
+
+  <!-- google analytics snnipet-->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-83147911-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<!-- end snnipet-->
 <br>
 <div class="panel panel-default" style="opacity: 0.8;">
   <div class="panel-heading">
